23a81fa12134ebe87c9e380f0962140ae5040f5b
kryptadia@gmail.com <kryptadia@gmail.com@173271d4-c6d8-11de-896c-518660fad6c5>
2010-05-14 19:17:11 +0000

    fait l import initial

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,13 +1,12 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
+require_once '/usr/share/php/symfony/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    // for compatibility / remove and enable only the plugins you want
-    $this->enableAllPluginsExcept(array('sfDoctrinePlugin', 'sfCompat10Plugin'));
+    $this->enablePlugins('sfDoctrinePlugin');
   }
 }
--- web/index.php
+++ web/index.php
@@ -3,5 +3,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('backend', 'prod', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('mainapp', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
