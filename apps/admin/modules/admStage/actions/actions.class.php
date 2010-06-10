fdc617e4e02b053e84ffdfe590259aab7e69cc70
kryptadia@gmail.com <kryptadia@gmail.com@173271d4-c6d8-11de-896c-518660fad6c5>
2010-06-10 19:58:32 +0000

    configuration de l'admin generator admStage

--- apps/admin/modules/admStage/actions/actions.class.php
+++ apps/admin/modules/admStage/actions/actions.class.php
@@ -13,4 +13,45 @@ require_once dirname(__FILE__).'/../lib/admStageGeneratorHelper.class.php';
  */
 class admStageActions extends autoAdmStageActions
 {
+  public function executeListImportStages()
+  {
+    $this->redirect('admStage/importcreate');
+  }
+  
+  public function executeImportcreate()
+  {
+    $this->redirect('admStage/index');
+  }
+
+  public function executeListMailRappel()
+  {
+    $this->gesseh_stages = Doctrine::getTable('GessehStage')->getActiveStages();
+    $lien = csSettings::get('BaseURL');
+    foreach($this->gesseh_stages as $stage)
+    {
+      $message = $this->getMailer()->compose(
+        array(csSettings::get('email') => csSettings::get('email_nom')),
+	  $stage->getGessehEtudiant()->getEmail(),
+	  '['.csSettings::get('email_prefixe').'] Rappel : évaluations à rendre',
+	  <<<EOF
+{$stage->getGessehEtudiant()->getPrenom()},
+
+L'évaluation en ligne du stage : {$stage->getGessehTerrain()->getFiliere()} à {$stage->getGessehTerrain()->getGessehHopital()->getNom()} du {$stage->getGessehPeriode()->getDebut()} au {$stage->getGessehPeriode()->getFin()} n'a pas été remplie.
+
+Nous vous rappelons que l'évaluation des stages est obligatoire et nous vous invitons à le faire dans les plus brefs délais en cliquant sur le lien suivant :
+
+{$lien}etudiant
+
+L'administration.
+
+Ce message a été généré automatiquement, merci de ne pas y répondre.
+EOF
+         );
+      $this->getMailer()->send($message);
+      $this->count++;
+    }
+    
+    $this->getUser()->setFlash('notice', sprintf('Vous venez d\'envoyer %s mails de rappel.', $this->count));
+    $this->redirect('admStage/index');
+  }
 }
