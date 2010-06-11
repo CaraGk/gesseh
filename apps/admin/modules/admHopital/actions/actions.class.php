285a3a2a39273108eb9f2da5b57a4a8032275408
kryptadia@gmail.com <kryptadia@gmail.com@173271d4-c6d8-11de-896c-518660fad6c5>
2010-06-11 13:03:06 +0000

    ajout du module admin/admHopital

---
+++ apps/admin/modules/admHopital/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/admHopitalGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/admHopitalGeneratorHelper.class.php';
+
+/**
+ * admHopital actions.
+ *
+ * @package    gesseh
+ * @subpackage admHopital
+ * @author     Pierre-François 'Pilou' Angrand <tmp@angrand.fr>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class admHopitalActions extends autoAdmHopitalActions
+{
+}
---
+++ apps/admin/modules/admHopital/lib/admHopitalGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * admHopital module configuration.
+ *
+ * @package    gesseh
+ * @subpackage admHopital
+ * @author     Pierre-François 'Pilou' Angrand <tmp@angrand.fr>
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class admHopitalGeneratorConfiguration extends BaseAdmHopitalGeneratorConfiguration
+{
+}
