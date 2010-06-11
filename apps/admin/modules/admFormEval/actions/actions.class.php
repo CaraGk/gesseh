deee7ba1b364f15830fb3bc298f99b10be80fa5b
kryptadia@gmail.com <kryptadia@gmail.com@173271d4-c6d8-11de-896c-518660fad6c5>
2010-06-11 13:03:52 +0000

    ajout du module admin/admFormEval

---
+++ apps/admin/modules/admFormEval/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/admFormEvalGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/admFormEvalGeneratorHelper.class.php';
+
+/**
+ * admFormEval actions.
+ *
+ * @package    gesseh
+ * @subpackage admFormEval
+ * @author     Pierre-François 'Pilou' Angrand <tmp@angrand.fr>
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class admFormEvalActions extends autoAdmFormEvalActions
+{
+}
