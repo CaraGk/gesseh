f4a6fa599d722121fd37704de21554a9baba48df
kryptadia@gmail.com <kryptadia@gmail.com@173271d4-c6d8-11de-896c-518660fad6c5>
2011-06-04 19:22:57 +0000

    Auto-génération des lib et import des étudiants et stage

---
+++ lib/filter/doctrine/GessehSimulationFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * GessehSimulation filter form.
+ *
+ * @package    gesseh
+ * @subpackage filter
+ * @author     Pierre-François Pilou Angrand <tmp@angrand.fr>
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class GessehSimulationFormFilter extends BaseGessehSimulationFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/GessehSimulationForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * GessehSimulation form.
+ *
+ * @package    gesseh
+ * @subpackage form
+ * @author     Pierre-François Pilou Angrand <tmp@angrand.fr>
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class GessehSimulationForm extends BaseGessehSimulationForm
+{
+  public function configure()
+  {
+  }
+}
