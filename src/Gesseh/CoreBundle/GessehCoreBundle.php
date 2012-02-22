b37ad7ea3e0f82b9e8887797d865da0581e6d2f3
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2012-02-22 19:35:45 +0100

    Tweak dégueulasse pour l'erreur DBALException:longblob

--- src/Gesseh/CoreBundle/GessehCoreBundle.php
+++ src/Gesseh/CoreBundle/GessehCoreBundle.php
@@ -3,7 +3,16 @@
 namespace Gesseh\CoreBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Doctrine\DBAL\Types\Type;
 
 class GessehCoreBundle extends Bundle
 {
+  public function boot() {
+    $em = $this->container->get('doctrine.orm.default_entity_manager');
+
+    Type::addType('longblob', 'Gesseh\CoreBundle\Doctrine\Types\LongBlobType');
+
+    $em->getConnection()->getDatabasePlatform()
+      ->registerDoctrineTypeMapping('LONGBLOB', 'longblob');
+  }
 }
