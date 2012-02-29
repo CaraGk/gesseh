6128a3fd40a70933e44e8df98f98312aa76aad51
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2012-03-01 00:34:29 +0100

    Configuration du FOSUserBundle

---
+++ src/Gesseh/UserBundle/Entity/User.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Gesseh\UserBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Gesseh\UserBundle\Entity\User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+  /**
+   * @var integer $id
+   *
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\generatedValue(strategy="AUTO")
+   */
+  protected $id;
+
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
