7c1ec75404d45ab9656197ce27a2dbf12bec055c
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2012-03-03 12:21:53 +0100

    Amélioriation du style des menus

--- src/Gesseh/CoreBundle/Menu/MenuBuilder.php
+++ src/Gesseh/CoreBundle/Menu/MenuBuilder.php
@@ -26,7 +26,7 @@ class MenuBuilder
     $menu = $this->factory->createItem('anon');
     $menu->setCurrentUri($request->getRequestUri());
 
-    $menu->addChild('Fieldset', array('route' => 'GCore_FSIndex'));
+    $menu->addChild('Fieldset', array('route' => 'GCore_FSIndex', 'label' => 'Terrains de stage'));
 
     return $menu;
   }
@@ -39,8 +39,8 @@ class MenuBuilder
     $menu = $this->factory->createItem('student');
     $menu->setCurrentUri($request->getRequestUri());
 
-    $menu->addChild('Fieldset', array('route' => 'GCore_FSIndex'));
-    $menu->addChild('Administrate', array('route' => 'admin_hp'));
+    $menu->addChild('Fieldset', array('route' => 'GCore_FSIndex', 'label' => 'Terrains de stage'));
+    $menu->addChild('Administrate', array('route' => 'admin_hp', 'label' => 'Administration'));
 
     return $menu;
   }
@@ -53,9 +53,9 @@ class MenuBuilder
     $menu = $this->factory->createItem('admin');
     $menu->setCurrentUri($request->getRequestUri());
 
-    $menu->addChild('Home', array('route' => 'homepage'));
-    $menu->addChild('Fieldset', array('route' => 'GCore_FSAIndex'));
-    $menu->addChild('Student', array('route' => 'GUser_SAIndex'));
+    $menu->addChild('Home', array('route' => 'homepage', 'label' => 'Retour'));
+    $menu->addChild('Fieldset', array('route' => 'GCore_FSAIndex', 'label' => 'Terrains de stage'));
+    $menu->addChild('Student', array('route' => 'GUser_SAIndex', 'label' => 'Étudiants'));
 
     return $menu;
   }
