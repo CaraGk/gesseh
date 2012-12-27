ddaf715ff15131ae034310aca75db2156493b266
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2012-12-27 20:14:10 +0100

    Adaptation du SensioDistributionBundle en GessehInstallBundle

--- src/Gesseh/InstallBundle/DependencyInjection/GessehInstallExtension.php
+++ src/Gesseh/InstallBundle/DependencyInjection/GessehInstallExtension.php
@@ -19,10 +19,20 @@ class GessehInstallExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+//        $configuration = new Configuration();
+//        $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
+        $loader->load('webconfigurator.xml');
+    }
+
+//    public function getNamespace()
+//    {
+//      return 'http://symfony.com/schema/dic/symfony/sensiodistribution';
+//    }
+
+    public function getAlias()
+    {
+      return 'gesseh_install';
     }
 }
--- src/Gesseh/InstallBundle/GessehInstallBundle.php
+++ src/Gesseh/InstallBundle/GessehInstallBundle.php
@@ -1,9 +1,32 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Gesseh\InstallBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Gesseh\InstallBundle\Configurator\Step\DoctrineStep;
+use Gesseh\InstallBundle\Configurator\Step\SecretStep;
 
+/**
+ * GessehInstallBundle.
+ *
+ * @author Fabien Potencier <fabien@symfony.com>
+ * @author Marc Weistroff <marc.weistroff@sensio.com>
+ */
 class GessehInstallBundle extends Bundle
 {
+    public function boot()
+    {
+        $configurator = $this->container->get('gesseh.install.webconfigurator');
+        $configurator->addStep(new DoctrineStep($configurator->getParameters()));
+        $configurator->addStep(new SecretStep($configurator->getParameters()));
+    }
 }
