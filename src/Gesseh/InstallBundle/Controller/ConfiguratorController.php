197f7a6b4e808d6314f749a5731eccc135a731b0
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2013-02-17 18:06:51 +0100

    #1 Premier jet fonctionnel de module d'installation

--- src/Gesseh/InstallBundle/Controller/ConfiguratorController.php
+++ src/Gesseh/InstallBundle/Controller/ConfiguratorController.php
@@ -13,6 +13,12 @@ namespace Gesseh\InstallBundle\Controller;
 
 use Symfony\Component\DependencyInjection\ContainerAware;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\DBAL\Migrations\Migration,
+  Doctrine\DBAL\Migrations\Configuration\Configuration;
+use Gesseh\ParameterBundle\Entity\Parameter;
+use Gesseh\UserBundle\Entity\Student,
+  Gesseh\UserBundle\Form\StudentType,
+  Gesseh\UserBundle\Form\StudentHandler;
 
 /**
  * ConfiguratorController.
@@ -94,4 +100,97 @@ class ConfiguratorController extends ContainerAware
             'is_writable' => $configurator->isFileWritable(),
         ));
     }
+
+    public function migrateAction()
+    {
+      $conn = $this->container->get('database_connection');
+      $configuration = new Configuration($conn);
+      $configuration->setMigrationsNamespace('Application\Migrations');
+      $configuration->setMigrationsDirectory('/home/pilou/Public/gesseh/app/DoctrineMigrations');
+      $configuration->registerMigrationsFromDirectory('/home/pilou/Public/gesseh/app/DoctrineMigrations');
+
+      $migration = new Migration($configuration);
+
+      $executedMigrations = $configuration->getMigratedVersions();
+      $availableMigrations = $configuration->getAvailableVersions();
+      $executedUnavailableMigrations = array_diff($executedMigrations, $availableMigrations);
+
+      if ($executedUnavailableMigrations) {
+        $warning = 'WARNING! You have ' . count($executedUnavailableMigrations) . ' previously executed migrations in the database that are not registered migrations : <ul>';
+        foreach ($executedUnavailableMigrations as $executedUnavailableMigration) {
+          $warning .= '<li>' . $executedUnavailableMigration . '</li>';
+        }
+        $warning .= '</ul>';
+        $infos[0] = $warning;
+      } else {
+        $sql = $migration->migrate(null, null);
+
+        if ( ! $sql) {
+          $infos[0] = 'No migrations to execute.';
+        } else {
+          $infos[0] = 'Création des tables réussie.';
+
+          // création et import des données de base : administrateur
+        }
+      }
+
+//      return new RedirectResponse($this->container->get('router')->generate('_configurator_final'));
+        return $this->container->get('templating')->renderResponse('GessehInstallBundle:Configurator:migrate.html.twig', array(
+          'sql'  => $sql,
+          'infos' => $infos,
+        ));
+    }
+
+    public function createConfigAction()
+    {
+      $parameters = array(
+        array(
+          'Name'     => 'title',
+          'Label'    => 'Nom du site',
+          'Category' => 'General',
+          'Type'     => 1,
+        ),
+        array(
+          'Name'     => 'eval_active',
+          'Label'    => 'Activer le module d\'évaluation',
+          'Category' => 'Evaluation',
+          'Type'     => 2,
+        ),
+        array(
+          'Name'     => 'simul_active',
+          'Label'    => 'Activer le module de simulation',
+          'Category' => 'Simulation',
+          'Type'     => 2,
+        ),
+      );
+
+      foreach($parameters as $parameter) {
+        $param = new Parameter();
+        foreach($parameter as $key=>$value) {
+          $func = 'set' . $key;
+          $param->$func($value);
+        }
+      }
+
+      return new RedirectResponse($this->container->get('router')->generate('_configurator_admin'));
+      $this->get('session')->setFlash('notice', 'Paramètres du site créés.');
+    }
+
+    public function createAdminAction()
+    {
+      $em = $this->container->get('doctrine')->getEntityManager();
+      $admin = new Student();
+      $form = $this->container->get('form.factory')->create(new StudentType(false), $admin);
+      $formHandler = new StudentHandler($form, $this->container->get('request'), $em, $this->container->get('fos_user.user_manager'));
+
+      if($formHandler->process()) {
+        $this->container->get('session')->setFlash('notice', 'Administrateur "' . $admin . '" enregistré.');
+        return new RedirectResponse($this->container->get('router')->generate('GCore_FSIndex'));
+      }
+
+      return $this->container->get('templating')->renderResponse('GessehInstallBundle:Configurator:admin.html.twig', array(
+        'admin_form' => $form->createView(),
+      ));
+    }
+
 }
