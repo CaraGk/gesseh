add8b73a4a5f777d3e4ace2fad50fbfbf451788f
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2013-08-07 17:36:52 +0200

    Fix autoload wrong path

--- app/autoload.php
+++ app/autoload.php
@@ -2,13 +2,11 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = include __DIR__.'/../vendor/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 
 // intl
 if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
-
-    $loader->add(array(__DIR__.'/../vendor/symfony/src/Symfony/Component/Locale/Resources/stubs'));
+    require_once __DIR__.'/../vendor/symfony/symfony/src/Symfony/Component/Locale/Resources/stubs/functions.php';
 }
 
 
