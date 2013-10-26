40820fc359e027b0101309e862112ffb9a449285
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2013-10-26 18:04:56 +0200

    #32 Ajout de la class confirm et amélioration du confirm.js

--- src/Gesseh/CoreBundle/Resources/public/js/confirm.js
+++ src/Gesseh/CoreBundle/Resources/public/js/confirm.js
@@ -9,7 +9,9 @@
 
 $(document).ready(function() {
   $('.confirm').click(function() {
-    if(confirm("Attention cette opération va supprimer définitivement l'élément ainsi que ceux qui en dépendent !\n\nSi vous rencontrez un message d'erreur, c'est probablement que certains de ces éléments liés ne peuvent être supprimés automatiquement. Vous devrez le faire manuellement.\n\nVoulez-vous vraiment continuer ?")) {
+    var msg;
+    if($(this).attr("confirm")){ msg=$(this).attr("confirm"); }else{ msg="Attention cette opération va supprimer définitivement l'élément ainsi que ceux qui en dépendent !\n\nSi vous rencontrez un message d'erreur, c'est probablement que certains de ces éléments liés ne peuvent être supprimés automatiquement. Vous devrez le faire manuellement.\n\nVoulez-vous vraiment continuer ?"; }
+    if(confirm(msg)) {
       return true;
     } else {
       return false;
