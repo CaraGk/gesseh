37a04e5b1bde341a368c9c1e826f1269f2ec0e82
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2014-09-14 02:36:03 +0200

    Fix #25 : ajout de l'import d'étudiants à partir d'un tableur

--- src/Gesseh/UserBundle/Controller/StudentAdminController.php
+++ src/Gesseh/UserBundle/Controller/StudentAdminController.php
@@ -14,9 +14,11 @@ namespace Gesseh\UserBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Gesseh\UserBundle\Entity\Student;
 use Gesseh\UserBundle\Form\StudentType;
 use Gesseh\UserBundle\Form\StudentHandler;
+use Gesseh\UserBundle\Entity\User;
 
 /**
  * StudentAdmin controller.
@@ -180,6 +182,151 @@ class StudentAdminController extends Controller
     $this->get('session')->getFlashBag()->add('notice', 'Droits d\'administration retirés à l\'étudiant "' . $student . '"');
 
     return $this->redirect($this->generateUrl('GUser_SAIndex'));
+  }
+
+  /**
+   * Import students from file into a grade
+   *
+   * @Route("/import", name="GUser_SAImport")
+   * @Template()
+   */
+  public function importAction(Request $request)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $um = $this->container->get('fos_user.user_manager');
+    $choices = array(
+        '0'  => '1re colonne',
+        '1'  => '2e colonne',
+        '2'  => '3e colonne',
+        '3'  => '4e colonne',
+        '4'  => '5e colonne',
+        '5'  => '6e colonne',
+        '6'  => '7e colonne',
+        '7'  => '8e colonne',
+        '8'  => '9e colonne',
+        '9'  => '10e colonne',
+        '10' => '11e colonne',
+        '11' => '12e colonne',
+        '12' => '13e colonne',
+        '13' => '14e colonne',
+        '14' => '15e colonne',
+        '15' => '16e colonne',
+    );
+
+    $form = $this->createFormBuilder()
+        ->add('file', 'file', array(
+            'label'    => 'Fichier',
+            'required' => true,
+        ))
+        ->add('surname', 'choice', array(
+            'label'    => 'Nom',
+            'required' => true,
+            'expanded' => false,
+            'multiple' => false,
+            'choices'  => $choices,
+        ))
+        ->add('name', 'choice', array(
+            'label'    => 'Prénom',
+            'required' => true,
+            'expanded' => false,
+            'multiple' => false,
+            'choices'  => $choices,
+        ))
+        ->add('phone', 'choice', array(
+            'label'    => 'Téléphone',
+            'required' => false,
+            'expanded' => false,
+            'multiple' => false,
+            'choices'  => $choices,
+            'empty_value' => 'aucune',
+            'empty_data'  => null,
+        ))
+        ->add('address', 'choice', array(
+            'label'    => 'Adresse',
+            'required' => false,
+            'expanded' => false,
+            'multiple' => false,
+            'choices'  => $choices,
+            'empty_value' => 'aucune',
+            'empty_data'  => null,
+        ))
+        ->add('email', 'choice', array(
+            'label'    => 'E-mail',
+            'required' => true,
+            'expanded' => false,
+            'multiple' => false,
+            'choices'  => $choices,
+        ))
+        ->add('ranking', 'choice', array(
+            'label'    => 'Classement ECN',
+            'required' => false,
+            'expanded' => false,
+            'multiple' => false,
+            'choices'  => $choices,
+            'empty_value' => 'aucune',
+            'empty_data'  => null,
+        ))
+        ->add('graduate', 'choice', array(
+            'label'    => 'Année ECN',
+            'required' => false,
+            'expanded' => false,
+            'multiple' => false,
+            'choices'  => $choices,
+            'empty_value' => 'aucune',
+            'empty_data'  => null,
+        ))
+        ->add('grade', 'entity', array(
+            'label'    => 'Promotion',
+            'required' => true,
+            'class'    => 'GessehUserBundle:Grade',
+        ))
+        ->add('Envoyer', 'submit')
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isValid()) {
+        $objPHPExcel = $this->get('phpexcel')->createPHPExcelObject($form['file']->getData())->setActiveSheetIndex();
+        $students_count = 2;
+
+        while ($objPHPExcel->getCellByColumnAndRow($form['surname']->getData(), $students_count)->getValue()) {
+            $student = new Student();
+            $student->setSurname($objPHPExcel->getCellByColumnAndRow($form['surname']->getData(), $students_count)->getValue());
+            $student->setName($objPHPExcel->getCellByColumnAndRow($form['name']->getData(), $students_count)->getValue());
+            if ($form['phone']->getData() != null)
+                $student->setPhone($objPHPExcel->getCellByColumnAndRow($form['phone']->getData(), $students_count)->getValue());
+            if ($form['address']->getData() != null)
+                $student->setAddress($objPHPExcel->getCellByColumnAndRow($form['address']->getData(), $students_count)->getValue());
+            if ($form['ranking']->getData() != null)
+                $student->setRanking($objPHPExcel->getCellByColumnAndRow($form['ranking']->getData(), $students_count)->getValue());
+            if ($form['graduate']->getData() != null)
+                $student->setGraduate($objPHPExcel->getCellByColumnAndRow($form['graduate']->getData(), $students_count)->getValue());
+            $student->setAnonymous(false);
+            $student->setGrade($form['grade']->getData());
+            $user = new User();
+            $um->createUser();
+            $user->setEmail($objPHPExcel->getCellByColumnAndRow($form['email']->getData(), $students_count)->getValue());
+            $user->setUsername($user->getEmail());
+            $user->setConfirmationToken(null);
+            $user->setEnabled(true);
+            $user->addRole('ROLE_STUDENT');
+            $user->setPlainPassword('tatatatata');
+            $student->setUser($user);
+            $em->persist($student);
+            $um->updateUser($user);
+            $students_count++;
+        }
+
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add('notice', $students_count - 2 . " ont été enregistrés dans la base de données.");
+
+        return $this->redirect($this->generateUrl('GUser_SAIndex'));
+    }
+
+    return array(
+        'form' => $form->createView(),
+    );
   }
 
 }
