b28687e8f5b50293bc21b7bbdd11ed90c21dd856
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2014-10-19 15:34:43 +0200

    #58 Affichage sommaire de l'évolution des évaluations numériques par semestre

--- src/Gesseh/EvaluationBundle/Controller/DefaultController.php
+++ src/Gesseh/EvaluationBundle/Controller/DefaultController.php
@@ -28,6 +28,8 @@ use Gesseh\EvaluationBundle\Form\EvaluationHandler;
 class DefaultController extends Controller
 {
     /**
+     * Affiche les évaluations d'un terrain de stage
+     *
      * @Route("/department/{id}", name="GEval_DShow", requirements={"id" = "\d+"})
      * @Template()
      */
@@ -36,34 +38,35 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $pm = $this->container->get('kdb_parameters.manager');
 
-      $student = $em->getRepository('GessehUserBundle:Student')->getByUsername($this->get('security.context')->getToken()->getUsername());
-      $current_period = $em->getRepository('GessehCoreBundle:Period')->getCurrent();
-      $count_placements = $em->getRepository('GessehCoreBundle:Placement')->getCountByStudentWithoutCurrentPeriod($student, $current_period);
-      if ($em->getRepository('GessehEvaluationBundle:Evaluation')->studentHasNonEvaluated($student, $current_period, $count_placements)) {
-          $this->get('session')->getFlashBag()->add('error', 'Il y a des évaluations non réalisées. Veuillez évaluer tous vos stages avant de pouvoir accéder aux autres évaluations.');
-          return $this->redirect($this->generateUrl('GCore_PIndex'));
-      }
-      $eval_limit = date('Y-m-d H:i:s', strtotime('-' . $pm->findParamByName('eval_limit')->getValue() . ' year'));
+        /* Vérification de l'évaluation de tous ses stages (sauf le courant) par l'étudiant */
+        $student = $em->getRepository('GessehUserBundle:Student')->getByUsername($this->get('security.context')->getToken()->getUsername());
+        $current_period = $em->getRepository('GessehCoreBundle:Period')->getCurrent();
+        $count_placements = $em->getRepository('GessehCoreBundle:Placement')->getCountByStudentWithoutCurrentPeriod($student, $current_period);
+        if ($em->getRepository('GessehEvaluationBundle:Evaluation')->studentHasNonEvaluated($student, $current_period, $count_placements)) {
+            $this->get('session')->getFlashBag()->add('error', 'Il y a des évaluations non réalisées. Veuillez évaluer tous vos stages avant de pouvoir accéder aux autres évaluations.');
+            return $this->redirect($this->generateUrl('GCore_PIndex'));
+        }
+        $eval_limit = date('Y-m-d H:i:s', strtotime('-' . $pm->findParamByName('eval_limit')->getValue() . ' year'));
 
-      $department = $em->getRepository('GessehCoreBundle:Department')->find($id);
+        $department = $em->getRepository('GessehCoreBundle:Department')->find($id);
 
-      if (!$department)
-        throw $this->createNotFoundException('Unable to find department entity.');
+        if (!$department)
+            throw $this->createNotFoundException('Unable to find department entity.');
 
-      $eval_text = $em->getRepository('GessehEvaluationBundle:Evaluation')->getTextByDepartment($id, $eval_limit);
-      $eval_num = $em->getRepository('GessehEvaluationBundle:Evaluation')->getNumByDepartment($id, $eval_limit);
+        $eval_text = $em->getRepository('GessehEvaluationBundle:Evaluation')->getTextByDepartment($id, $eval_limit);
+        $eval_num = $em->getRepository('GessehEvaluationBundle:Evaluation')->getNumByDepartment($id, $eval_limit);
 
-      if ($eval_sector = $em->getRepository('GessehEvaluationBundle:EvalSector')->getEvalSector($department->getSector()->getId()))
-        $eval_form = $eval_sector->getForm();
-      else
-        throw $this->createNotFoundException('Aucun formulaire d\'évaluation attribué à ce stage ! Veuillez contacter un administrateur.');
+        if ($eval_sector = $em->getRepository('GessehEvaluationBundle:EvalSector')->getEvalSector($department->getSector()->getId()))
+            $eval_form = $eval_sector->getForm();
+        else
+            throw $this->createNotFoundException('Aucun formulaire d\'évaluation attribué à ce stage ! Veuillez contacter un administrateur.');
 
-      return array(
-        'eval_text'  => $eval_text,
-        'eval_num'   => $eval_num,
-        'department' => $department,
-        'eval_form'  => $eval_form,
-      );
+        return array(
+            'eval_text'  => $eval_text,
+            'eval_num'   => $eval_num,
+            'department' => $department,
+            'eval_form'  => $eval_form,
+        );
     }
 
     /**
--- src/Gesseh/EvaluationBundle/Entity/EvaluationRepository.php
+++ src/Gesseh/EvaluationBundle/Entity/EvaluationRepository.php
@@ -18,64 +18,93 @@ use Doctrine\ORM\EntityRepository;
  */
 class EvaluationRepository extends EntityRepository
 {
-  public function getEvaluationQuery()
-  {
-    return $this->createQueryBuilder('e')
-                ->join('e.placement', 'p')
-                ->join('p.department', 'd')
-                ->join('e.evalCriteria', 'c')
-    ;
-  }
+    /*
+     * Generic query with joins
+     *
+     * returns QueryBuilder
+     */
+    public function getEvaluationQuery()
+    {
+        return $this->createQueryBuilder('e')
+            ->join('e.placement', 'p')
+            ->join('p.department', 'd')
+            ->join('e.evalCriteria', 'c')
+        ;
+    }
 
-  public function getTextByDepartment($id, $date = null)
-  {
-    $query = $this->getEvaluationQuery();
-    $query->where('d.id = :id')
+    /*
+     * Récupère les évaluations textuelles (texte long) d'un terrain de stage
+     *
+     * returns QueryResult
+     */
+    public function getTextByDepartment($id, $date = null)
+    {
+        $query = $this->getEvaluationQuery();
+        $query->where('d.id = :id')
             ->setParameter('id', $id)
             ->andWhere('c.type = 2')
             ->andWhere('not(c.moderate = true and e.moderated = false)')
-          ->addOrderBy('c.rank', 'asc')
-          ->addOrderBy('e.created_at', 'asc');
+            ->addOrderBy('c.rank', 'asc')
+            ->addOrderBy('e.created_at', 'asc')
+        ;
 
-    if ($date != null) {
-      $query->andWhere('e.created_at > :date')
-            ->setParameter('date', $date);
+        if ($date != null) {
+            $query->andWhere('e.created_at > :date')
+                ->setParameter('date', $date)
+            ;
+        }
+
+        return $query->getQuery()->getResult();
     }
 
-    return $query->getQuery()->getResult();
-  }
-
-  public function getNumByDepartment($id, $date = null)
-  {
-    $query = $this->getEvaluationQuery();
-    $query->where('d.id = :id')
+    /*
+     * Récupère les évaluations numériques d'un terrain de stage
+     *
+     * returns QueryResult
+     */
+    public function getNumByDepartment($id, $date = null)
+    {
+        $query = $this->getEvaluationQuery();
+        $query->join('p.period', 'q')
+            ->where('d.id = :id')
             ->setParameter('id', $id)
-          ->andWhere('c.type = 1')
-          ->orderBy('c.name', 'asc');
+            ->andWhere('c.type = 1')
+            ->addOrderBy('c.name', 'asc')
+            ->addOrderBy('q.begin', 'asc')
+        ;
 
-    if ($date != null) {
-      $query->andWhere('e.created_at > :date')
-            ->setParameter('date', $date);
+        if ($date != null) {
+            $query->andWhere('e.created_at > :date')
+                ->setParameter('date', $date)
+            ;
+        }
+
+        $results = $query->getQuery()->getResult();
+        $calc = array();
+
+        foreach ($results as $result) {
+            if (!isset($calc[$result->getEvalCriteria()->getId()]['count'][0])) {
+                $calc[$result->getEvalCriteria()->getId()]['total'][0] = 0;
+                $calc[$result->getEvalCriteria()->getId()]['count'][0] = 0;
+                $calc[$result->getEvalCriteria()->getId()]['name'] = $result->getEvalCriteria()->getName();
+            }
+
+            if (!isset($calc[$result->getEvalCriteria()->getId()]['count'][$result->getPlacement()->getPeriod()->getId()])) {
+                $calc[$result->getEvalCriteria()->getId()]['total'][$result->getPlacement()->getPeriod()->getId()] = 0;
+                $calc[$result->getEvalCriteria()->getId()]['count'][$result->getPlacement()->getPeriod()->getId()] = 0;
+            }
+
+            $calc[$result->getEvalCriteria()->getId()]['total'][0] += (int) $result->getValue();
+            $calc[$result->getEvalCriteria()->getId()]['count'][0] ++;
+            $calc[$result->getEvalCriteria()->getId()]['mean'][0] = round($calc[$result->getEvalCriteria()->getId()]['total'][0] / $calc[$result->getEvalCriteria()->getId()]['count'][0], 1);
+//            $calc[$result->getEvalCriteria()->getId()]['ratio'][0] = $result->getEvalCriteria()->getRatio();
+            $calc[$result->getEvalCriteria()->getId()]['total'][$result->getPlacement()->getPeriod()->getId()] += (int) $result->getValue();
+            $calc[$result->getEvalCriteria()->getId()]['count'][$result->getPlacement()->getPeriod()->getId()] ++;
+            $calc[$result->getEvalCriteria()->getId()]['mean'][$result->getPlacement()->getPeriod()->getId()] = round($calc[$result->getEvalCriteria()->getId()]['total'][$result->getPlacement()->getPeriod()->getId()] / $calc[$result->getEvalCriteria()->getId()]['count'][$result->getPlacement()->getPeriod()->getId()], 1);
+        }
+
+        return $calc;
     }
-
-    $results = $query->getQuery()->getResult();
-    $calc = array();
-
-    foreach ($results as $result) {
-      if (!isset($calc[$result->getEvalCriteria()->getId()]['count'])) {
-        $calc[$result->getEvalCriteria()->getId()]['total'] = 0;
-        $calc[$result->getEvalCriteria()->getId()]['count'] = 0;
-        $calc[$result->getEvalCriteria()->getId()]['name'] = $result->getEvalCriteria()->getName();
-      }
-
-      $calc[$result->getEvalCriteria()->getId()]['total'] += (int) $result->getValue();
-      $calc[$result->getEvalCriteria()->getId()]['count'] ++;
-      $calc[$result->getEvalCriteria()->getId()]['mean'] = round($calc[$result->getEvalCriteria()->getId()]['total'] / $calc[$result->getEvalCriteria()->getId()]['count'], 1);
-//      $calc[$result->getEvalCriteria()->getId()]['ratio'] = $result->getEvalCriteria()->getRatio();
-    }
-
-    return $calc;
-  }
 
   public function getEvaluatedList($type = 'array', $username = null)
   {
