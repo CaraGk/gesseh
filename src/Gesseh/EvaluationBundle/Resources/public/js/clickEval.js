d5f6489e6b77de418d8e628da5847871ce0c5e94
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2014-10-24 00:52:19 +0200

    #59 Mise en page CoreBundle:Placement:index

---
+++ src/Gesseh/EvaluationBundle/Resources/public/js/clickEval.js
@@ -0,0 +1,17 @@
+/**
+ * This file is part of GESSEH project
+ *
+ * @author: Pierre-François Angrand <caragk@angrand.fr>
+ * @copyright: Copyright 2013 Pierre-François Angrand
+ * @license: GPLv3
+ * See LICENSE file or http://www.gnu.org/licenses/gpl.html
+*/
+
+$(document).ready(function() {
+    $('.evaluated').parent('ul').parent('li.entity').addClass('valid').addClass('mouseHand');
+    $('.nonevaluated').parent('ul').parent('li.entity').addClass('invalid').addClass('mouseHand');
+
+    $('.placement').on('click', function() {
+        $(location).attr('pathname', $(this).children('ul.actions').children('li').children('a').attr('href'));
+    });
+});
