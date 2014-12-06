fdf7c7a0126d6bbd6fc6210a857d121b9176b335
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2014-12-06 01:57:14 +0100

    Fix #95 Amélioration de switchSeparator.js

--- src/Gesseh/UserBundle/Resources/public/js/switchSeparator.js
+++ src/Gesseh/UserBundle/Resources/public/js/switchSeparator.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-    var pattern = {
-        ' ;' : /,/gi,
-        ',' : / ;/gi,
-    };
+    var pattern = new RegExp(", ", "gi");
 
     $('#separator').on('change', function() {
-        $('#showlist').text($('#showlist').text().replace(pattern[$(this).val()], $(this).val()));
+        if($(this).val() == "retour") {
+            $('#showlist').text($('#showlist').text().replace(pattern, "\n"));
+            pattern = new RegExp("\n", "gi");
+        } else {
+            $('#showlist').text($('#showlist').text().replace(pattern, $(this).val()));
+            pattern = new RegExp($(this).val(), "gi");
+        }
     });
 });
