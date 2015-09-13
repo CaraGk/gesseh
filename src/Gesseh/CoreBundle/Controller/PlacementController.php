a455347837ac5dd91986adcd49898736d02953ee
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2015-09-13 18:20:09 +0200

    Changement de l'adresse mail de référence pour la licence

--- src/Gesseh/CoreBundle/Controller/PlacementController.php
+++ src/Gesseh/CoreBundle/Controller/PlacementController.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of GESSEH project
  *
- * @author: Pierre-François ANGRAND <caragk@angrand.fr>
+ * @author: Pierre-François ANGRAND <gesseh@medlibre.fr>
  * @copyright: Copyright 2013 Pierre-François Angrand
  * @license: GPLv3
  * See LICENSE file or http://www.gnu.org/licenses/gpl.html
