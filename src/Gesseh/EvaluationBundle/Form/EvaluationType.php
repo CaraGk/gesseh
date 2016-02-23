38fd1cf382a6c207105008a5db2bc088232641af
Pierre-François Pilou Angrand <caragk@medlibre.fr>
2016-02-23 11:30:39 +0100

    #125 Meilleure lisibilité du statut isPrivate des évaluations

--- src/Gesseh/EvaluationBundle/Form/EvaluationType.php
+++ src/Gesseh/EvaluationBundle/Form/EvaluationType.php
@@ -31,18 +31,22 @@ class EvaluationType extends AbstractType
         foreach ($this->eval_forms as $eval_form)
         {
             foreach ($eval_form->getCriterias() as $criteria) {
-                if ($criteria->isPrivate() == true)
-                    $label = $criteria->getName() . "(réservée aux étudiants seuls)";
-                else
-                    $label = $criteria->getName() . "(peut-être transmise aux enseignants)";
+                if ($criteria->isPrivate() == true) {
+                    $label = $criteria->getName() . " (réservée aux étudiants seuls)";
+                    $class = 'private';
+                } else {
+                    $label = $criteria->getName() . " (peut-être transmise aux enseignants)";
+                    $class = 'public';
+                }
 
                 if ($criteria->getType() == 1) {
                     $builder->add('criteria_' . $criteria->getId(), 'choice', array(
-                        'choices'  => $this->getCriteriaSubjectiveChoiceOptions($criteria->getMore()),
-                        'required' => $criteria->isRequired(),
-                        'multiple' => false,
-                        'expanded' => true,
-                        'label'    => $label,
+                        'choices'    => $this->getCriteriaSubjectiveChoiceOptions($criteria->getMore()),
+                        'required'   => $criteria->isRequired(),
+                        'multiple'   => false,
+                        'expanded'   => true,
+                        'label'      => $label,
+                        'label_attr' => array('class' => $class),
                     ));
                 } elseif ($criteria->getType() == 2) {
                     $builder->add('criteria_' . $criteria->getId(), 'textarea', array(
@@ -50,28 +54,32 @@ class EvaluationType extends AbstractType
                         'trim'       => true,
                         'max_length' => 250,
                         'label'      => $label,
+                        'label_attr' => array('class' => $class),
                     ));
                 } elseif ($criteria->getType() == 3) {
                     $builder->add('criteria_' . $criteria->getId(), 'choice', array(
-                        'choices'  => $this->getCriteriaChoiceOptions($criteria->getMore()),
-                        'required' => $criteria->isRequired(),
-                        'multiple' => true,
-                        'expanded' => true,
-                        'label'    => $label,
+                        'choices'    => $this->getCriteriaChoiceOptions($criteria->getMore()),
+                        'required'   => $criteria->isRequired(),
+                        'multiple'   => true,
+                        'expanded'   => true,
+                        'label'      => $label,
+                        'label_attr' => array('class' => $class),
                     ));
                 } elseif ($criteria->getType() == 4) {
                     $builder->add('criteria_' . $criteria->getId(), 'integer', array(
-                        'precision' => $criteria->getMore(),
-                        'required'  => $criteria->isRequired(),
-                        'label'     => $label,
+                        'precision'  => $criteria->getMore(),
+                        'required'   => $criteria->isRequired(),
+                        'label'      => $label,
+                        'label_attr' => array('class' => $class),
                     ));
                 } elseif ($criteria->getType() == 5) {
                     $builder->add('criteria_' . $criteria->getId(), 'choice', array(
-                        'choices'  => $this->getCriteriaChoiceOptions($criteria->getMore(), array(0)),
-                        'required' => $criteria->isRequired(),
-                        'multiple' => false,
-                        'expanded' => true,
-                        'label'    => $label,
+                        'choices'    => $this->getCriteriaChoiceOptions($criteria->getMore(), array(0)),
+                        'required'   => $criteria->isRequired(),
+                        'multiple'   => false,
+                        'expanded'   => true,
+                        'label'      => $label,
+                        'label_attr' => array('class' => $class),
                     ));
                 } elseif ($criteria->getType() == 6) {
                     $builder->add('criteria_' . $criteria->getId(), 'time', array(
@@ -80,6 +88,7 @@ class EvaluationType extends AbstractType
                         'with_seconds' => false,
                         'required'     => $criteria->isRequired(),
                         'label'        => $label,
+                        'label_attr'   => array('class' => $class),
                     ));
                 }
             }
