f33706382b4f0b2b2a8ccf3f466927a805875f9e
Pierre-François Pilou Angrand <caragk@medlibre.fr>
2016-03-12 22:39:44 +0100

    Fix #147 switch du test sur usename vers id pour le UserHandler

--- src/Gesseh/UserBundle/Form/StudentHandler.php
+++ src/Gesseh/UserBundle/Form/StudentHandler.php
@@ -4,7 +4,7 @@
  * This file is part of GESSEH project
  *
  * @author: Pierre-François ANGRAND <gesseh@medlibre.fr>
- * @copyright: Copyright 2013 Pierre-François Angrand
+ * @copyright: Copyright 2013-2016 Pierre-François Angrand
  * @license: GPLv3
  * See LICENSE file or http://www.gnu.org/licenses/gpl.html
  */
@@ -22,74 +22,74 @@ use FOS\UserBundle\Doctrine\UserManager;
  */
 class StudentHandler
 {
-  private $form;
-  private $request;
-  private $em;
-  private $um;
+    private $form;
+    private $request;
+    private $em;
+    private $um;
 
-  public function __construct(Form $form, Request $request, EntityManager $em, UserManager $um, $admin = false)
-  {
-    $this->form    = $form;
-    $this->request = $request;
-    $this->em      = $em;
-    $this->um      = $um;
-    $this->admin   = $admin;
-  }
-
-  public function process()
-  {
-    if ( $this->request->getMethod() == 'POST' ) {
-      $this->form->bind($this->request);
-
-      if ($this->form->isValid()) {
-        $this->onSuccess(($this->form->getData()));
-
-        return true;
-      }
+    public function __construct(Form $form, Request $request, EntityManager $em, UserManager $um, $admin = false)
+    {
+        $this->form    = $form;
+        $this->request = $request;
+        $this->em      = $em;
+        $this->um      = $um;
+        $this->admin   = $admin;
     }
 
-    return false;
-  }
+    public function process()
+    {
+        if ( $this->request->getMethod() == 'POST' ) {
+            $this->form->bind($this->request);
 
-  public function onSuccess(Student $student)
-  {
-    $this->updateUser($student->getUser());
-    $student->setAnonymous(false);
-    $this->em->persist($student);
-    $this->em->flush();
-  }
+            if ($this->form->isValid()) {
+                $this->onSuccess(($this->form->getData()));
 
-  private function updateUser($user)
-  {
-    if ( null == $user->getUsername() ) {
-      $this->um->createUser();
-      if (!$user->getPlainPassword()) {
-        $user->setPlainPassword($this->generatePwd(8));
-      } else {
-        $user->addRole('ROLE_ADMIN');
-      }
-      if ($this->admin) {
-          $user->addRole('ROLE_ADMIN');
-      }
-      $user->setConfirmationToken(null);
-      $user->setEnabled(true);
-      $user->addRole('ROLE_STUDENT');
-    }
-    $user->setUsername($user->getEmail());
+                return true;
+            }
+        }
 
-    $this->um->updateUser($user);
-  }
-
-  private function generatePwd($length)
-  {
-    $characters = array ('a','z','e','r','t','y','u','p','q','s','d','f','g','h','j','k','m','w','x','c','v','b','n','2','3','4','5','6','7','8','9','A','Z','E','R','T','Y','U','P','S','D','F','G','H','J','K','L','M','W','X','C','V','B','N');
-    $password = '';
-
-    for ($i = 0 ; $i < $length ; $i++) {
-      $rand = array_rand($characters);
-      $password .= $characters[$rand];
+        return false;
     }
 
-    return $password;
-  }
+    public function onSuccess(Student $student)
+    {
+        $this->updateUser($student->getUser());
+        $student->setAnonymous(false);
+        $this->em->persist($student);
+        $this->em->flush();
+    }
+
+    private function updateUser($user)
+    {
+        if (null == $user->getId()) {
+            $this->um->createUser();
+            if (!$user->getPlainPassword()) {
+                $user->setPlainPassword($this->generatePwd(8));
+            } else {
+                $user->addRole('ROLE_ADMIN');
+            }
+            if ($this->admin) {
+                $user->addRole('ROLE_ADMIN');
+            }
+            $user->setConfirmationToken(null);
+            $user->setEnabled(true);
+            $user->addRole('ROLE_STUDENT');
+        }
+        $user->setUsername($user->getEmail());
+
+        $this->um->updateUser($user);
+    }
+
+    private function generatePwd($length)
+    {
+        $characters = array ('a','z','e','r','t','y','u','p','q','s','d','f','g','h','j','k','m','w','x','c','v','b','n','2','3','4','5','6','7','8','9','A','Z','E','R','T','Y','U','P','S','D','F','G','H','J','K','L','M','W','X','C','V','B','N');
+        $password = '';
+
+        for ($i = 0 ; $i < $length ; $i++) {
+            $rand = array_rand($characters);
+            $password .= $characters[$rand];
+        }
+
+        return $password;
+    }
 }
