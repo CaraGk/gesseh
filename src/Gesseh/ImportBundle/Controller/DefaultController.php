219dee9a3bc7654e1bfdb8662ae49ca30c48077f
Pierre-François Pilou Angrand <caragk@medlibre.fr>
2016-09-01 16:58:32 +0200

    Securisation avec PreAuthorize

--- src/Gesseh/ImportBundle/Controller/DefaultController.php
+++ src/Gesseh/ImportBundle/Controller/DefaultController.php
@@ -21,7 +21,7 @@ use Symfony\Component\HttpFoundation\ResponseHeaderBag;
  * site de l'AIMGL.
  *
  * @Route("/admin/import")
- * @Security\Secure(roles="ROLE_ADMIN")
+ * @Security\PreAuthorize("hasRole('ROLE_ADMIN')")
  */
 
 class DefaultController extends Controller
