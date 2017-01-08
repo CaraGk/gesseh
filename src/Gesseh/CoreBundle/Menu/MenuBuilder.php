ea0a81c306ec35cea19bdc3875e2005b3c30461c
Pierre-François Pilou Angrand <kryptadia@gmail.com>
2017-01-08 19:23:37 +0100

    Passage du menu en navbar-nav et amélioration du menu et de sa présentation

--- src/Gesseh/CoreBundle/Menu/MenuBuilder.php
+++ src/Gesseh/CoreBundle/Menu/MenuBuilder.php
@@ -4,7 +4,7 @@
  * This file is part of GESSEH project
  *
  * @author: Pierre-François ANGRAND <gesseh@medlibre.fr>
- * @copyright: Copyright 2013-2015 Pierre-François Angrand
+ * @copyright: Copyright 2013-2017 Pierre-François Angrand
  * @license: GPLv3
  * See LICENSE file or http://www.gnu.org/licenses/gpl.html
  */
@@ -12,7 +12,10 @@
 namespace Gesseh\CoreBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\RequestStack,
+    Symfony\Component\Security\Core\Authorization\AuthorizationChecker;
 use Symfony\Component\HttpFoundation\Request;
+use KDB\ParametersBundle\Model\ParameterManager;
 
 /**
  * Menu builder class
@@ -28,6 +31,63 @@ class MenuBuilder
   {
     $this->factory = $factory;
   }
+
+    /**
+     * @param RequestStack $requestStack
+     * @param AuthorizationChecker $security
+     */
+    public function createMainMenu(RequestStack $requestStack, AuthorizationChecker $security, ParameterManager $pm)
+    {
+        $menu = $this->factory->createItem('main', array('navbar' => true));
+        $session = $requestStack->getCurrentRequest()->getSession();
+
+        $menu->addChild('Fieldset', array('route' => 'GCore_FSIndex', 'label' => 'Terrains de stage', 'attributes' => array('title' => 'Liste des terrains de stage')));
+
+        if (!$security->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            if ($pm->findParamByName('reg_active')->getValue() and $pm->findParamByName('reg_auto')->getValue()) {
+                $menu->addChild('Register', array('route' => 'GRegister_URegister', 'label' => 'S\'inscrire', 'attributes' => array('title' => 'S\'inscrire et adhérer')));
+            }
+            $menu->addChild('Login', array('route' => 'fos_user_security_login', 'label' => 'S\'identifier', 'attributes' => array('title' => 'S\'identifier pour accéder au site')));
+            $menu->addChild('LostPassword', array('route' => 'fos_user_resetting_request', 'label' => 'Mot de passe oublié', 'attributes' => array('title' => 'Réinitialiser le mot de passe')));
+        } else {
+            if ($security->isGranted('ROLE_STUDENT')) {
+                $menu->addChild('Myself', array('route' => 'GUser_SShow', 'label' => 'Mon profil', 'attributes' => array('title' => 'Mes informations personnelles, mes stages en cours ou effectués, etc.')));
+            }
+
+            if ($security->isGranted('ROLE_ADMIN')) {
+                $adminMenu = $menu->addChild('Adminstration', array('label' => 'Administrer', 'dropdown' => true, 'caret' => true, 'icon' => 'king'));
+                $adminMenu->addChild('Sectors', array('route' => 'GCore_FSASector', 'label' => 'Catégories', 'attributes' => array('title' => 'Gérer les catégories')));
+                $adminMenu->addChild('Students', array('route' => 'GUser_SAIndex', 'label' => 'Étudiants', 'attributes' => array('title' => 'Gérer les étudiants')));
+                $adminMenu->addChild('Grades', array('route' => 'GUser_GAIndex', 'label' => 'Promotions', 'attributes' => array('title' => 'Gérer les promotions')));
+                $adminMenu->addChild('Placements', array('route' => 'GCore_PAPlacementIndex', 'label' => 'Stages', 'attributes' => array('title' => 'Gérer les stages')));
+                $adminMenu->addChild('Periods', array('route' => 'GCore_PAPeriodIndex', 'label' => 'Périodes de stage', 'attributes' => array('title' => 'Gérer les périodes de stage')));
+                if ($pm->findParamByName('simul_active')->getValue()) {
+                    $adminMenu->addChild('Simulation', array('route' => 'GSimul_SAList', 'label' => 'Simulations', 'attributes' => array('title' => 'Gérer les simulations')));
+                    $adminMenu->addChild('SimRules', array('route' => 'GSimul_SARule', 'label' => 'Règles de simulation', 'attributes' => array('title' => 'Gérer les règles de simulation')));
+                }
+                if ($pm->findParamByName('eval_active')->getValue()) {
+                    $adminMenu->addChild('EvalForms', array('route' => 'GEval_AIndex', 'label' => 'Formulaires d\'évaluation', 'attributes' => array('title' => 'Gérer les formulaires d\'évaluation de stage')));
+                    $adminMenu->addChild('Moderation', array('route' => 'GEval_ATextIndex', 'label' => 'Modérer', 'attributes' => array('title' => 'Modérer les évaluations de stage')));
+                }
+                $adminMenu->addChild('Parameters', array('route' => 'GParameter_PAIndex', 'label' => 'Paramètres', 'attributes' => array('title' => 'Gérer les paramètres du site')));
+            }
+
+            if ($security->isGranted('ROLE_TEACHER')) {
+                $menu->addChild('My department', array('route' => 'GCore_FSIndex', 'routeParameters' => array('limit' => array('type' => 'u.id', 'value' => '', 'description' => '')), 'label' => 'Mes terrains', 'attributes' => array('title' => 'Afficher les terrains de stage dont je suis responsable')));
+            }
+
+            if ($security->isGranted('ROLE_SUPERTEACHER')) {
+                $adminMenu = $menu->addChild('Adminstration', array('label' => 'Administrer', 'dropdown' => true, 'caret' => true, 'icon' => 'king'));
+                $adminMenu->addChild('Students', array('route' => 'GUser_SAIndex', 'label' => 'Étudiants', 'attributes' => array('title' => 'Gérer les étudiants')));
+                $adminMenu->addChild('EvalForms', array('route' => 'GEval_AIndex', 'label' => 'Formulaires d\'évaluation', 'attributes' => array('title' => 'Gérer les formulaires d\'évaluation de stage')));
+                $adminMenu->addChild('Moderation', array('route' => 'GEval_ATextIndex', 'label' => 'Modérer', 'attributes' => array('title' => 'Modérer les évaluations de stage')));
+            }
+
+            $menu->addChild('Logout', array('route' => 'fos_user_security_logout', 'label' => 'Se déconnecter', 'attributes' => array('title' => 'Se déconnecter du site'), 'icon' => 'log-out'));
+        }
+
+        return $menu;
+    }
 
   /**
    * @param Request $request
