fb7f5bb118ceef18ace0b4e52748a3cb07e56d0b
Pierre-François Pilou Angrand <caragk@medlibre.fr>
2019-08-11 20:26:44 +0200

    Change parameter for compatibility with PIGASS

---
+++ app/DoctrineMigrations/Version20190811174523.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190811174523 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE parameter DROP activates_at, DROP expires_at');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE parameter ADD activates_at DATETIME DEFAULT NULL, ADD expires_at DATETIME DEFAULT NULL');
+    }
+}
